01089d1f004d04ce68084fce715bc0a8545e7fca
Orlin Nedyalkov <orlinbox@gmail.com>
2019-01-27 00:21:23 +0200

    Release version 1.0

--- simple-modal.js
+++ simple-modal.js
@@ -44,16 +44,18 @@ SM version 1.0
     }
   });
   // custom vh for mobile devices (due to address bar height)
-  function customvh() { document.documentElement.style.setProperty('--vh', window.innerHeight*0.01 + 'px'); }
+  function customvh() { document.documentElement.style.setProperty('--smvh', window.innerHeight*0.01 + 'px'); }
   // orientation change
-  window.addEventListener('orientationchange', function() {
-    customvh();
-    var afterOrientationChange = function() {
+  if ($('.js-simple-modal').length) {
+    window.addEventListener('orientationchange', function() {
       customvh();
-      window.removeEventListener('resize', afterOrientationChange);
-    };
-    window.addEventListener('resize', afterOrientationChange);
-  });
+      var afterOrientationChange = function() {
+        customvh();
+        window.removeEventListener('resize', afterOrientationChange);
+      };
+      window.addEventListener('resize', afterOrientationChange);
+    });
+  }
   // open function
   function opensm(el, label) {
     customvh();
